75675497bd822a4fe691f8a1bb9235bf8ce05c96
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-24 15:02:32 -0500

    I modified 'act3_task1.rb', as well as that file's 'sample_text' value.

--- act3_task1.rb
+++ act3_task1.rb
@@ -4,11 +4,13 @@
 
 
 def extract_urls(text)
-    pattern = /(http|https|ftp):\/\/(www)?/
+    pattern = /((http|https|ftp):\/\/(www)?.+)/
     url_list = text.scan(pattern)
     puts url_list
 end
 
-sample_text = "Visit our site at http://www.example.org for more information."
+sample_text = "Visit our site at http://www.example.org for more information.
+Check out our search page at https://example.com/search?q=ruby+regex. Don't
+forget to ftp our resources at ftp://example.com/resources."
 
 extract_urls(sample_text)
